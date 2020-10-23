571821aaa94bbfec0806881ae730f3a56eeaa1da
Karol Wawrzecki <kwawrzecki@gmail.com>
2020-10-23 16:40:13 +0200

    add cross origin for user blueprint

--- src/routes/user.py
+++ src/routes/user.py
@@ -3,10 +3,14 @@ Defines the blueprint for the users
 """
 from flask import Blueprint
 from flask_restful import Api
+from flask_cors import CORS
 
 from resources import UserResource
 
 USER_BLUEPRINT = Blueprint("user", __name__)
+
+CORS(USER_BLUEPRINT)
+
 Api(USER_BLUEPRINT).add_resource(
     UserResource, "/user/<string:name>"
 )
