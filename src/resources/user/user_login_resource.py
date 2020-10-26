2756b4a606429848606bf9ebe544f4308b388756
tjaygallery <tjaygallery@gmail.com>
2020-10-26 18:34:00 +0100

    validation fix

--- src/resources/user/user_login_resource.py
+++ src/resources/user/user_login_resource.py
@@ -15,7 +15,7 @@ class UserLoginResource(Resource):
     )
     def post(email: str, password: str):
         user = UserRepository.find_by_email(email)
-        if email != user.email or not check_password_hash(user.password, password):
+        if not user or email != user.email or not check_password_hash(user.password, password):
             return {"message": "Bad username or password"}, 400
         access_token = create_access_token(identity=email)
         return {"access_token": access_token}, 200
