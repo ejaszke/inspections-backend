a74e42f74f68c25c4a2f121ef9a12745e1fac675
tjaygallery <tjaygallery@gmail.com>
2020-10-26 18:21:40 +0100

    check password hash fix

--- src/resources/user/user_login_resource.py
+++ src/resources/user/user_login_resource.py
@@ -3,7 +3,7 @@ from flask_jwt_extended import create_access_token
 from util import parse_params
 from flask_restful.reqparse import Argument
 from repositories import UserRepository
-from werkzeug.security import safe_str_cmp
+from werkzeug.security import check_password_hash
 
 
 class UserLoginResource(Resource):
@@ -15,7 +15,7 @@ class UserLoginResource(Resource):
     )
     def post(email: str, password: str):
         user = UserRepository.find_by_email(email)
-        if email != user.email or safe_str_cmp(password, user.password):
+        if email != user.email or not check_password_hash(user.password, password):
             return {"message": "Bad username or password"}, 400
         access_token = create_access_token(identity=email)
         return {"access_token": access_token}, 200
