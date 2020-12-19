966bd9fd5198d2c048472e1ec6cfd2335d5c1b8d
Edward Jaszke <edward.jaszke@imagene.me>
2020-12-19 12:15:06 +0100

    Fix qr code size

--- src/resources/inspection/inspection_pdfs.py
+++ src/resources/inspection/inspection_pdfs.py
@@ -7,18 +7,17 @@ from repositories import InspectionRepository
 import qrcode
 
 
-
 class InspectionPdfResource(Resource):
     @staticmethod
-    def get(type:str, id: str):
-        inspection = InspectionRepository.find_by_id(id)
-        img = qrcode.make('test text')
+    def get(pdf_type: str, inspection_id: str):
+        inspection = InspectionRepository.find_by_id(inspection_id)
+        img = qrcode.make('https://inspections-frontend.herokuapp.com/#/inspections/' + inspection_id + '/confirmations/new')
         img.save('src/static/img/inspection_qrcode.png')
-        template = 'inspection.html' if type == 'adm' else 'inspection-dg.html'
+        template = 'inspection.html' if pdf_type == 'adm' else 'inspection-dg.html'
         html = render_template(
             template,
             inspection=inspection,
             current_date=datetime.now(),
-            days=["niedziela", "poniedziałek", "wtorek", "środa", "czwartek", "piątek", "sobota"]
+            days=["poniedziałek", "wtorek", "środa", "czwartek", "piątek", "sobota", "niedziela"]
         )
         return render_pdf(HTML(string=html))
