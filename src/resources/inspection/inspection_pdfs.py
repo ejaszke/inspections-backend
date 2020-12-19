a6cff43d82bfcff28f922a9d785d5f6c2ddf2a1a
Edward Jaszke <edward.jaszke@imagene.me>
2020-12-19 15:32:40 +0100

    Change qr code png save method

--- src/resources/inspection/inspection_pdfs.py
+++ src/resources/inspection/inspection_pdfs.py
@@ -2,6 +2,8 @@ from flask import render_template
 from flask_restful import Resource
 from flask_weasyprint import render_pdf, HTML
 from datetime import datetime
+import base64
+from io import BytesIO
 
 from repositories import InspectionRepository
 import qrcode
@@ -14,10 +16,17 @@ class InspectionPdfResource(Resource):
         img = qrcode.make('https://inspections-frontend.herokuapp.com/#/inspections/' + id + '/confirmations/new')
         img.save('inspection_qrcode.png')
         template = 'inspection.html' if type == 'adm' else 'inspection-dg.html'
+
+        buf = BytesIO()
+        img.save(buf, 'png')
+        buf.seek(0)
+
         html = render_template(
             template,
             inspection=inspection,
             current_date=datetime.now(),
-            days=["poniedziałek", "wtorek", "środa", "czwartek", "piątek", "sobota", "niedziela"]
+            days=["poniedziałek", "wtorek", "środa", "czwartek", "piątek", "sobota", "niedziela"],
+            qr_code=base64.b64encode(buf.read()).decode("utf-8")
         )
+        buf.close()
         return render_pdf(HTML(string=html))
