aeb47edf2647c770e989e6dab061e990091fc61b
t-janicki <36083556+t-janicki@users.noreply.github.com>
2021-01-23 09:32:23 +0100

    Bugfix/auth exp handler (#15)
    * Add new inspection pdf
    
    * Fix qr code size
    
    * Fix qr code size on second pdf
    
    * Add inspection qr code png to git ignore
    
    * Fix CORS settings
    
    * Disable bascic log config
    
    * Set origins
    
    * Change pdf endpoint attribute names
    
    * Change img path
    
    * Change qr code png save method
    
    * Remove saving qr code
    
    * Change adm template
    
    * Set token expiration
    
    * config
    
    Co-authored-by: Edward Jaszke <edward.jaszke@imagene.me>
    Co-authored-by: Edward Jaszke <edward.jaszke@gmail.com>

--- src/resources/inspection/inspection_pdfs.py
+++ src/resources/inspection/inspection_pdfs.py
@@ -2,22 +2,30 @@ from flask import render_template
 from flask_restful import Resource
 from flask_weasyprint import render_pdf, HTML
 from datetime import datetime
+import base64
+from io import BytesIO
 
 from repositories import InspectionRepository
 import qrcode
 
 
-
 class InspectionPdfResource(Resource):
     @staticmethod
-    def get(id: str):
+    def get(type: str, id: str):
         inspection = InspectionRepository.find_by_id(id)
-        img = qrcode.make('test text')
-        img.save('src/static/img/inspection_qrcode.png')
+        img = qrcode.make('https://inspections-frontend.herokuapp.com/#/inspections/' + id + '/confirmations/new')
+        template = 'inspection.html' if type == 'adm' else 'inspection-dg.html'
+
+        buf = BytesIO()
+        img.save(buf, 'png')
+        buf.seek(0)
+
         html = render_template(
-            'inspection.html',
+            template,
             inspection=inspection,
             current_date=datetime.now(),
-            days=["niedziela", "poniedziałek", "wtorek", "środa", "czwartek", "piątek", "sobota"]
+            days=["poniedziałek", "wtorek", "środa", "czwartek", "piątek", "sobota", "niedziela"],
+            qr_code=base64.b64encode(buf.read()).decode("utf-8")
         )
+        buf.close()
         return render_pdf(HTML(string=html))
